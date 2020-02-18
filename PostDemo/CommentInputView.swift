8798c53c1e4086ba97a999cb309f7b406eb086b8
xiaoyouxinqing <xiaoyouxinqing@foxmail.com>
2020-02-18 10:33:50 -0800

    Clean code

--- PostDemo/CommentInputView.swift
+++ PostDemo/CommentInputView.swift
@@ -21,49 +21,48 @@ struct CommentInputView: View {
     @ObservedObject private var keyboardResponder = KeyboardResponder()
     
     var body: some View {
-        ZStack {
-            VStack(spacing: 0) {
-                CommentTextView(text: $text, beginEditingOnAppear: true)
-                
-                HStack(spacing: 0) {
-                    Button(action: {
-                        self.presentationMode.wrappedValue.dismiss()
-                    }) {
-                        Text("取消")
-                            .padding()
-                    }
-                    
-                    Spacer()
-                    
-                    Button(action: {
-                        if self.text.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
-                            self.showEmptyTextHUD = true
-                            
-                            DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
-                                self.showEmptyTextHUD = false
-                            }
-                            return
-                        }
-                        print(self.text)
-                        var post = self.post
-                        post.commentCount += 1
-                        self.userData.update(post)
-                        self.presentationMode.wrappedValue.dismiss()
-                    }) {
-                        Text("发送")
-                            .padding()
-                    }
-                }
-                .font(.system(size: 18))
-                .foregroundColor(.black)
-            }
+        VStack(spacing: 0) {
+            CommentTextView(text: $text, beginEditingOnAppear: true)
             
+            HStack(spacing: 0) {
+                Button(action: {
+                    self.presentationMode.wrappedValue.dismiss()
+                }) {
+                    Text("取消")
+                        .padding()
+                }
+                
+                Spacer()
+                
+                Button(action: {
+                    if self.text.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
+                        self.showEmptyTextHUD = true
+                        
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+                            self.showEmptyTextHUD = false
+                        }
+                        return
+                    }
+                    print(self.text)
+                    var post = self.post
+                    post.commentCount += 1
+                    self.userData.update(post)
+                    self.presentationMode.wrappedValue.dismiss()
+                }) {
+                    Text("发送")
+                        .padding()
+                }
+            }
+            .font(.system(size: 18))
+            .foregroundColor(.black)
+        }
+        .overlay(
             Text("评论不能为空")
                 .scaleEffect(showEmptyTextHUD ? 1 : 0.5)
                 .animation(.spring(dampingFraction: 0.5))
                 .opacity(showEmptyTextHUD ? 1 : 0)
                 .animation(.easeInOut)
-        }
+        )
         .padding(.bottom, keyboardResponder.keyboardHeight)
         .edgesIgnoringSafeArea(keyboardResponder.keyboardShow ? .bottom : [])
     }
